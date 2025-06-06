4971932a751dda5d568b3ef59d17b7e9472b2901
Raul Jimenez Ortega <rjimenez@esri.com>
2025-06-07 00:40:56 +0200

    Fixing axios and local dev problems

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/vibrant-community-reveal/',
+  base: mode === 'production' ? '/vibrant-community-reveal/' : '/',
   server: {
     host: "::",
     port: 8080,
